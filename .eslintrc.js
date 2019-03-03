f0ae214fd9e114357c7ed11de9c62ea829742fd1
vomvoru <qkrcjfgus3@gmail.com>
2019-03-03 21:29:21 +0900

    [#1] 복잡도, 코드라인수, depth 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
     jest: true
   },
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }]
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+    complexity: ["error", { max: 10 }],
+    "max-lines": [
+      "error",
+      { max: 100, skipBlankLines: true, skipComments: true }
+    ],
+    "max-depth": ["error", 2]
   }
 };
