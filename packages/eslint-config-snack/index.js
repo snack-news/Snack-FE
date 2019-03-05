ed9c4a97894c26af02d4dcd9b6a0e720bf10f2c5
vomvoru <qkrcjfgus3@gmail.com>
2019-03-06 03:09:41 +0900

    [#1] esilnt 규칙 typescript에서 문제되는 사항 수정

--- packages/eslint-config-snack/index.js
+++ packages/eslint-config-snack/index.js
@@ -1,3 +1,6 @@
+const airbnbBaseStyleRules = require('eslint-config-airbnb-base/rules/style').rules;
+const airbnbBaseVariablesRules = require('eslint-config-airbnb-base/rules/variables').rules;
+
 module.exports = {
   extends: ['airbnb'],
   parser: '@typescript-eslint/parser',
@@ -24,5 +27,38 @@ module.exports = {
     'max-lines': ['error', { max: 100, skipBlankLines: true, skipComments: true }],
     'max-depth': ['error', 2],
     'import/no-extraneous-dependencies': 'off',
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': airbnbBaseStyleRules.camelcase,
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    indent: 'off',
+    '@typescript-eslint/indent': airbnbBaseStyleRules.indent,
+    '@typescript-eslint/interface-name-prefix': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': airbnbBaseStyleRules['no-array-constructor'],
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 'error',
+    '@typescript-eslint/no-parameter-properties': 'error',
+    '@typescript-eslint/no-triple-slash-reference': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': airbnbBaseVariablesRules['no-unused-vars'],
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/prefer-interface': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/type-annotation-spacing': 'error',
   },
 };
