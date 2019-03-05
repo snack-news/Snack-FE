c722c6f7b553454b6a38b0943e61ff660ba3673b
vomvoru <qkrcjfgus3@gmail.com>
2019-03-06 02:17:46 +0900

    [#1] eslint resolver 설정 추가, 및 현재 모노레포로 문제되는 eslint 규칙 off

--- packages/eslint-config-snack/index.js
+++ packages/eslint-config-snack/index.js
@@ -2,6 +2,13 @@ module.exports = {
   extends: ['airbnb'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
@@ -16,5 +23,6 @@ module.exports = {
     complexity: ['error', { max: 10 }],
     'max-lines': ['error', { max: 100, skipBlankLines: true, skipComments: true }],
     'max-depth': ['error', 2],
+    'import/no-extraneous-dependencies': 'off',
   },
 };
