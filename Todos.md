ca9fab223c03d85f848f411f15257a7db530d69e
vomvoru <qkrcjfgus3@gmail.com>
2019-05-29 01:28:14 +0900

    [선택과 집중] 모두 삭제. eslint 세팅

--- Todos.md
+++
@@ -1,3 +0,0 @@
-해야될 일.
-
-- https://reacttraining.com/react-router/web/guides/quick-start.
--- packages/client/ui/WeeklySelector.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-const WeeklySelector = () => <section>1월 2주</section>;
-
-export default WeeklySelector;
