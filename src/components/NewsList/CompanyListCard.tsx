4a610977dbca963ec4a55c3bcc36c010d55cd94a
vomvoru <qkrcjfgus3@gmail.com>
2019-06-07 22:48:20 +0900

    회사 목록 카드 마크업 개발중.

--- src/components/NewsList/CompanyListCard.tsx
+++ src/components/NewsList/CompanyListCard.tsx
@@ -3,6 +3,16 @@ import styled from 'styled-components';
 
 import HorizontalDivider from '../HorizontalDivider';
 
+const CompanyBox = () => <CompanyBox.Wrapper />;
+CompanyBox.Wrapper = styled.div`
+  width: 89px;
+  height: 74px;
+  border-radius: 5px;
+  border: solid 0.5px #d6d6db;
+  flex: none;
+  margin-right: 10px;
+`;
+
 const CompanyListCard = () => (
   <React.Fragment>
     <HorizontalDivider thick />
@@ -11,6 +21,16 @@ const CompanyListCard = () => (
         <CompanyListCard.Title />
         <CompanyListCard.MoreLink />
       </CompanyListCard.Header>
+      <CompanyListCard.Body>
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+        <CompanyBox />
+      </CompanyListCard.Body>
     </CompanyListCard.Wrapper>
     <HorizontalDivider thick />
   </React.Fragment>
@@ -18,16 +38,15 @@ const CompanyListCard = () => (
 
 CompanyListCard.Wrapper = styled.div`
   display: flex;
-  padding: 15px 0;
+  padding: 15px 20px 10px 20px;
   flex-direction: column;
-  align-items: center;
 `;
 
 CompanyListCard.Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  padding-bottom: 5px;
 `;
 
 CompanyListCard.Title = styled.div.attrs({ children: '회사별 뉴스 모아보기' })`
@@ -41,6 +60,16 @@ CompanyListCard.MoreLink = styled.div.attrs({ children: '모두보기' })`
   font-family: AppleSDGothicNeo;
   font-size: 13px;
   color: #0b66f7;
+`;
+
+CompanyListCard.Body = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  flex-wrap: nowrap;
+  overflow: auto;
+  padding: 10px 0;
 `;
 
 export default CompanyListCard;
