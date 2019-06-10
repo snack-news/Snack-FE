b108b4ad419ba2a0c89218264821f6bee2b37b9d
vomvoru <qkrcjfgus3@gmail.com>
2019-06-11 01:13:11 +0900

    SocialListCard 마크업 완료

--- src/components/NewsList/SocialListCard.tsx
+++ src/components/NewsList/SocialListCard.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { SFC } from 'react';
 import styled from 'styled-components';
 
 import HorizontalDivider from '../HorizontalDivider';
+
+const SocialLinkCard: SFC<{}> = ({ children }) => (
+  <SocialLinkCardWrapper>
+    <SocialLinkCardLabel>
+      {children}
+      <br />
+      에서 보기
+    </SocialLinkCardLabel>
+  </SocialLinkCardWrapper>
+);
+
+const SocialLinkCardWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 81px;
+  height: 75px;
+  border-radius: 5px;
+  background-color: #33599d;
+  margin-right: 10px;
+  flex: none;
+`;
+
+const SocialLinkCardLabel = styled.div`
+  font-family: SFProDisplay;
+  font-size: 12px;
+  font-weight: 500;
+  text-align: center;
+  color: #fefefe;
+`;
 
 const SocialListCard = () => (
   <React.Fragment>
@@ -9,8 +40,19 @@ const SocialListCard = () => (
     <SocialListCard.Wrapper>
       <SocialListCard.Header>
         <SocialListCard.Title />
-        <SocialListCard.MoreLink />
       </SocialListCard.Header>
+      <SocialListCard.Body>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+        <SocialLinkCard>FaceBook</SocialLinkCard>
+      </SocialListCard.Body>
     </SocialListCard.Wrapper>
     <HorizontalDivider thick />
   </React.Fragment>
@@ -18,29 +60,32 @@ const SocialListCard = () => (
 
 SocialListCard.Wrapper = styled.div`
   display: flex;
-  padding: 15px 0;
+  padding: 0 20px;
   flex-direction: column;
-  align-items: center;
 `;
 
 SocialListCard.Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  margin: 15px 0;
 `;
 
-SocialListCard.Title = styled.div.attrs({ children: '회사별 뉴스 모아보기' })`
+SocialListCard.Title = styled.div.attrs({ children: '스낵뉴스를 원하는 플랫폼에서 챙겨보세요' })`
   font-family: SFProDisplay;
   font-size: 17px;
   font-weight: 600;
   color: #000000;
 `;
 
-SocialListCard.MoreLink = styled.div.attrs({ children: '모두보기' })`
-  font-family: AppleSDGothicNeo;
-  font-size: 13px;
-  color: #0b66f7;
+SocialListCard.Body = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  flex-wrap: nowrap;
+  overflow: auto;
+  margin: 5px 0 15px 0;
 `;
 
 export default SocialListCard;
