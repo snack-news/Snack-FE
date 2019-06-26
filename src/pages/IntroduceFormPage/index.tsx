9cbe1d4e34eb4cb8711687a98c3e33f6cdd05e5d
martinyounghoonkim <hoons0131@gmail.com>
2019-06-26 23:17:40 +0900

    TextField 컴포넌트 추가

--- src/pages/IntroduceFormPage/index.tsx
+++ src/pages/IntroduceFormPage/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BothMarginWrapper } from 'Layouts/index';
 
 import {
   Header,
   HorizontalDivider,
   Footer,
   TextCard,
+  TextField
 } from 'Templates/index';
 
 const Wrapper = styled.div``;
@@ -17,6 +19,9 @@ const PicksPage = () => (
     <TextCard
       title="Snack에 소개하고 싶은 소식이나 링크를 알려주세요!"
     />
+    <BothMarginWrapper depth={2}>
+      <TextField />
+    </BothMarginWrapper>
     <HorizontalDivider thick />
     <Footer />
   </Wrapper>
