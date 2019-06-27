4c47bfc5f6970956a254b0af1160f353d4b34405
vomvoru <qkrcjfgus3@gmail.com>
2019-06-27 20:40:08 +0900

    TextField props 및 옵션 변경

--- src/templates/TextField/index.tsx
+++ src/templates/TextField/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const TextField = () => (
-  <input type="text"/>
-);
+const InputElement = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 13px 20px;
+  border: solid 0.5px #93939f;
+  background-color: #fefefe;
+  font-size: 12px;
+  &:focus {
+    outline: none;
+  }
+`;
 
-export default TextField;
+export default InputElement;
