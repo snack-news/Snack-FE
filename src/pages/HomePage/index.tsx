100ae09af4f13bf833795751ce88a8e02181492c
vomvoru <qkrcjfgus3@gmail.com>
2019-08-18 06:19:40 +0900

    [#112] 오늘을 기준으로 year, month, week 값 구하는 로직 추가

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,6 +7,24 @@ export const HomePage: FunctionComponent = props => {
 };
 
 const getFilterOnToday = () => {
+  const today = new Date();
+  const year = `${today.getFullYear()}`;
+  const month = `${today.getMonth() + 1}`.padStart(2, '0');
+  const week = `${getWeek(today)}`.padStart(2, '0');
+
   // TODO 오늘을 기준으로 보여줘야 되는 yyyy년 mm월 ww주 구해서 NewsListPage에 전달.
-  return { year: '2019', month: '08', week: '03' };
+  return { year, month, week };
 };
+
+const getWeek = (dateInstance: Date): number => {
+  const date = dateInstance.getDate();
+  const year = dateInstance.getFullYear();
+  const month = dateInstance.getMonth();
+
+  const firstDate = new Date(year, month);
+  const firstWeekLength = 7 - getDayStartMonday(firstDate);
+
+  return firstWeekLength >= date ? 1 : Math.ceil((date - firstWeekLength) / 7) + 1;
+};
+
+const getDayStartMonday = (d: Date) => (d.getDay() || 7) - 1;
