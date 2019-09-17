905e229c6b0f6dd20f7f8ab014b4d10fdc6f8a47
vomvoru <qkrcjfgus3@gmail.com>
2019-09-18 05:00:13 +0900

    댓글달기 기능 추가

--- .github/actions/bundle-size/entrypoint.sh
+++ .github/actions/bundle-size/entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-ls -lha public/dist/js/*.js  | awk '{print $5,$9}'
+DATA=$(ls -lha build/static/js/*.js  | awk '{print $5,$9}')
+echo $DATA
+
+URL=$(cat /github/workflow/event.json | jq -r .pull_request._links.comments.href)
+
+COMMENT="### Bundle Size:
+$DATA
+"
+
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+
+curl -d "$PAYLOAD" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -X POST "$URL"
