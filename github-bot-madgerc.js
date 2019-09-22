63ca99bcd8d71ac1b8b3f493411ae64eb791d621
vomvoru <qkrcjfgus3@gmail.com>
2019-09-22 19:09:58 +0900

    update github-bot-madgerc

--- github-bot-madgerc.js
+++ github-bot-madgerc.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   src: path.resolve(__dirname, './src/index.tsx'),
   webpackConfig: path.resolve(__dirname, './webpack.config.resolve.js'),
+  fileExtensions: ['js', 'ts', 'jsx', 'tsx'],
 };
