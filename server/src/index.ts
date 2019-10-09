af5b4964804a0f8a226a6ac8506fff94153c82d1
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2019-10-10 04:09:11 +0900

    [#252] port 를 env로 받는 형태로 수정

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import path from 'path';
 import { render } from './render';
 
 const API_URL = 'https://snak.news';
+const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 
@@ -39,6 +40,6 @@ app.use(ctx => {
   ctx.body = render(location);
 });
 
-app.listen(3000);
+app.listen(PORT);
 
-console.log('Server running on port 3000');
+console.log(`Server running on port ${PORT}`);
