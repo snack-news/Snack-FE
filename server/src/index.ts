49b134f2d7504e0214d6c3be7db4483713880b1f
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2019-10-13 05:00:12 +0900

    [#252] 서버 코드 수정 - koa-proxies 는 type 을 제공하지 않아서 koa-proxy로 변경 - debug 사용 - 매직넘버 삭제 (getPort 사용)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,47 +1,42 @@
 import Koa from 'koa';
 import serve from 'koa-static';
-import proxy from 'koa-proxies';
+import proxy from 'koa-proxy';
+import getPort from 'get-port';
 
 import path from 'path';
 
+import { config } from 'dotenv';
+
+import { debug } from './debug';
 import { render } from './render';
 
-import { env } from '~root/utils/env';
+config();
 
 const API_URL = 'https://snak.news';
-const PORT = env.PORT || 3000;
 
 const app = new Koa();
 
-app.use(
-  proxy('/api', {
-    target: API_URL,
-    changeOrigin: true,
+app
+  .use(proxy({ host: API_URL, match: /^\/api\// }))
+  .use(async (ctx, next) => {
+    if (ctx.path === '/') {
+      ctx.body = render(ctx.path);
+      return;
+    }
+
+    await next();
   })
-);
+  .use(serve(path.resolve(__dirname, '../../client/dist/')))
+  .use(ctx => {
+    ctx.body = render(ctx.path);
+  });
 
-// 경로가 / 일시에 index.html 을 전달하는게 아닌, 서버사이드 렌더링 작업을 합니다
-app.use((ctx, next) => {
-  if (ctx.path === '/') {
-    // 요청이 들어올 때 현재 경로를 render 함수에 전달시켜서 문자열을 생성합니다
-    const location = ctx.path;
-    ctx.body = render(location);
-    return;
-  }
+const run = async () => {
+  const PORT = process.env.PORT || (await getPort());
 
-  return next();
-});
+  app.listen(PORT);
 
-// 파일을 요청 받으면 build 내부의 파일들을 반환합니다
-app.use(serve(path.resolve(__dirname, '../../client/dist/')));
+  debug(`Server running on port ${PORT}`);
+};
 
-// 경로가 / 일시에 index.html 을 전달하는게 아닌, 서버사이드 렌더링 작업을 합니다
-app.use(ctx => {
-  // 요청이 들어올 때 현재 경로를 render 함수에 전달시켜서 문자열을 생성합니다
-  const location = ctx.path;
-  ctx.body = render(location);
-});
-
-app.listen(PORT);
-
-console.log(`Server running on port ${PORT}`);
+run();
