6ad650f1c645a23756e02cfc5bf0cca30f0538a7
YoungJae Kwon <kyoje11@gmail.com>
2019-10-31 23:43:15 +0900

    Update Deploy Script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -40,17 +40,17 @@ rm -rf ${IDLE_PATH}
 echo "> 새로 업데이트 된 소스를 쉬고 있는 Profile에 복사합니다"
 mkdir -p ${IDLE_PATH} && cp -R ${BUILD_PATH}/* ${IDLE_PATH}
 
-# cd ${IDLE_PATH}
-# echo "> ${IDLE_PATH}"
-# echo "> 어플리케이션 INSALL"
-# sudo npm install
+cd ${IDLE_PATH}
+echo "> ${IDLE_PATH}"
+echo "> 어플리케이션 INSALL"
+sudo npm install
 
-# echo "> 어플리케이션 BUILD"
-# sudo npm run build
-# sleep 2
+echo "> 어플리케이션 BUILD"
+sudo npm run build
+sleep 2
 
 echo "> 어플리케이션 RUN"
-sudo PORT=${IDLE_PORT} pm2 start server/dist/index.js --name ${IDLE_PROFILE}
+sudo PORT=${IDLE_PORT} pm2 start ${IDLE_PATH}/server/dist/index.js --name ${IDLE_PROFILE}
 
 echo "> Nginx 스위칭"
 sleep 2
