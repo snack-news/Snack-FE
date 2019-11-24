3958de0ecec0332f82e48b07ff27f657075e37d2
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2019-11-24 19:04:44 +0900

    썸네일 크기 동일하게 개선
    썸네일 크기 동일하게 개선

--- client/src/components/ExternalLink/LinkImg.tsx
+++ client/src/components/ExternalLink/LinkImg.tsx
@@ -1,58 +1,32 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
 
 import { MetaData } from './useLinkMetaData';
 
 import { Center } from '~client/layouts';
 
 interface ILinkImgProps {
-  label: string;
   meta: MetaData | null;
 }
 
-const LinkImg: FunctionComponent<ILinkImgProps> = ({ label, meta }) => {
-  if (meta && meta.image) {
-    return (
-      <Center>
-        <img
-          src={meta.image}
-          style={{
-            maxWidth: '360px',
-            maxHeight: '180px',
-          }}
-          alt="link"
-        />
-      </Center>
-    );
+const LinkImg: FunctionComponent<ILinkImgProps> = ({ meta }) => {
+  if (!(meta && meta.image)) {
+    return null;
   }
 
-  return <ShuffleBackgroundColorBox label={label} />;
-};
-
-interface IShuffleBackgroundColorBoxProps {
-  label: string;
-}
-
-const ShuffleBackgroundColorBox: FunctionComponent<
-  IShuffleBackgroundColorBoxProps
-> = ({ label }) => {
   return (
-    <div style={{ width: '100%', height: '160px', backgroundColor: '#4a4a4a' }}>
-      <Label>{label}</Label>
-    </div>
+    <Center>
+      <div
+        style={{
+          width: '100%',
+          height: '180px',
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage: `url('${meta.image}')`,
+          backgroundSize: 'cover',
+        }}
+      />
+    </Center>
   );
 };
-
-const Label = styled.div`
-  position: relative;
-  top: 20px;
-  left: 25px;
-
-  width: 170px;
-  height: 36px;
-  font-size: 14px;
-  font-weight: bold;
-  color: #fefefe;
-`;
 
 export default LinkImg;
