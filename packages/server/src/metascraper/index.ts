34de90a473ffa19553a609791161f9a229e52759
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-01-18 02:19:48 +0900

    fake browser User-Agent값 설정하여 메타태그 못가져오는 사이트에서 가져올수 있도록 개선
    - ex) http://www.zdnet.co.kr/view/?no=20191220172116

--- packages/server/src/metascraper/index.ts
+++ packages/server/src/metascraper/index.ts
@@ -53,6 +53,8 @@ export const getMetadata = async (targetUrl: string) => {
       responseType: 'arraybuffer',
       headers: {
         Accept: '*/*',
+        'User-Agent':
+          ' Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
       },
     })
     .catch(error => {
