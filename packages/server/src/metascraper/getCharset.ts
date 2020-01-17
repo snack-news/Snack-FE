0e7805a4725e847ae567fe22485ddf4e61cea4b6
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-01-18 03:15:13 +0900

    Content-Type으로 charset이 설정된 경우의 charset 값 추출 로직 추가
    - ex) https://www.mk.co.kr/news/business/view/2020/01/33916/

--- packages/server/src/metascraper/getCharset.ts
+++ packages/server/src/metascraper/getCharset.ts
@@ -5,6 +5,8 @@ export const getCharset = (response: AxiosResponse) => {
 
   if (!charset) charset = getCharsetWithMetaTag(response);
 
+  if (!charset) charset = getCharsetWithContentTypeMetaTag(response);
+
   return charset || 'utf-8';
 };
 
@@ -16,7 +18,15 @@ const getCharsetWithHeaders = (response: AxiosResponse) => {
 };
 
 const getCharsetWithMetaTag = (response: AxiosResponse) => {
+  // ex: <meta charset="euc-kr">
   const charset = /charset="([^";]+)/gi.exec(response.data.toString());
+
+  return charset === null || charset.length < 2 ? undefined : charset[1];
+};
+
+const getCharsetWithContentTypeMetaTag = (response: AxiosResponse) => {
+  // ex: <meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+  const charset = /(?:"| )charset=([^";]+)/gi.exec(response.data.toString());
 
   return charset === null || charset.length < 2 ? undefined : charset[1];
 };
