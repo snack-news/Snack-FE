1e81c07748696e396dfa74351d9440c355be271e
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-02-10 06:10:41 +0900

    카카오 URL 변경

--- packages/client/src/constants/urls.ts
+++ packages/client/src/constants/urls.ts
@@ -1,6 +1,7 @@
 export const FACEBOOK_URL = 'https://www.facebook.com/groups/snacknews/';
 export const TWITTER_URL = 'https://twitter.com/techsnacknews';
-export const KAKAO_URL = 'https://open.kakao.com/o/gKIXUx0';
+export const KAKAO_URL =
+  'https://docs.google.com/forms/d/1FT4haTK-8lmHqh3tJOffMgF77FRzRwIls1jwElp0EvQ/viewform?edit_requested=true';
 export const EMAIL_URL = 'mailto:teamsnackofficial@gmail.com';
 export const CONTACT_URL = 'https://seanlion.typeform.com/to/p5I7gf';
 export const CONTACT_NAME = '문의하기';
