863e1d19627c358fa52e33af223e9fc7bf83070a
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-02-09 12:59:49 +0900

    corp 뉴스 페이지가 있는 것만 링크 연결

--- packages/client/src/view/components/NewsItem/NewsLabelList/TopicLabelList.tsx
+++ packages/client/src/view/components/NewsItem/NewsLabelList/TopicLabelList.tsx
@@ -6,19 +6,32 @@ import styled from 'styled-components';
 import { NewsLabel } from './NewsLabel';
 
 import { CORPS_NEWS_PATH } from '~src/constants/paths';
+import { useCorps } from '~src/hooks/useCorps';
 
 interface IProps {
   topics: ITopic[];
 }
 
-export const TopicLabelList: FC<IProps> = ({ topics }) => (
-  <>
-    {topics.map(topic => (
-      <LinkWrap to={`${CORPS_NEWS_PATH}/${topic.id}`} key={topic.id}>
-        <NewsLabel>{topic.name}</NewsLabel>
-      </LinkWrap>
-    ))}
-  </>
-);
+export const TopicLabelList: FC<IProps> = ({ topics }) => {
+  const corps = useCorps();
 
-const LinkWrap = styled(Link)``;
+  return (
+    <>
+      {topics.map(topic => {
+        if (corps?.some(({ id }) => id === topic.id)) {
+          return (
+            <LinkWrap to={`${CORPS_NEWS_PATH}/${topic.id}`} key={topic.id}>
+              <NewsLabel>{topic.name}</NewsLabel>
+            </LinkWrap>
+          );
+        }
+
+        return <NewsLabel key={topic.id}>{topic.name}</NewsLabel>;
+      })}
+    </>
+  );
+};
+
+const LinkWrap = styled(Link)`
+  text-decoration: none;
+`;
