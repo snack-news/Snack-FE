75d756bd2277d62e3119a258ccb1ae0e3f1c07cd
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2020-02-16 11:01:46 +0900

    뒤로갈수 없을시 뒤로가기 버튼을 로고로 변경

--- packages/client/src/view/components/Header/Anchors/BackAnchor.tsx
+++ packages/client/src/view/components/Header/Anchors/BackAnchor.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router';
+
+import { LogoAnchor } from './LogoAnchor';
 
 import { backBlueArrowImg } from '~src/resources';
 import { useGoBack } from '~src/hooks/useGoBack';
 import { ImgButton } from '~src/view/components/ImgButton';
 
 export const BackAnchor: FC = () => {
+  const history = useHistory();
   const goBack = useGoBack();
-  return <BackBlueArrowButton onClick={goBack} />;
+
+  const canGo = !!history.location.key;
+
+  return canGo ? <BackBlueArrowButton onClick={goBack} /> : <LogoAnchor />;
 };
 
 const BackBlueArrowButton = styled(ImgButton).attrs({ src: backBlueArrowImg, width: '25px' })`
